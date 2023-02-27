b39d1557a6b155647fc6d639f0bc903c8cfb889e
Pocius <Lukas.pocevicius@gmail.com>
2023-02-27 19:56:25 +0200

    dev and start commands

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router=express.Router()
-const auth=require ("../middleware/auth")
 
 const{LOGIN,SIGN_UP}= require("../controllers/user")
 
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 
 mongoose.set("strictQuery", false);
 mongoose
